b11a698caf823935f5f1450a6fe164ae9a32f9a2
VarvaraMassalskaya <masalski0n@gmail.com>
2019-03-07 09:27:30 +0300

    lab01_z08

---
+++ lab01_z08.sh/lab1_z08.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#!/bin/bash
+if [ -n $1 ] && [ "$1"  == "-task" ];then
+  echo "Задание"
+  echo "8. Вывести номер последнего запущенного в фоне процесса."
+  echo
+  echo
+  fi
+echo "$!"
+exit 0
