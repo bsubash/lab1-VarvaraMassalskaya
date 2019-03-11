40fc0b13ab32d870fd3ff4c55e052fe5d9ad9374
VarvaraMassalskaya <masalski0n@gmail.com>
2019-03-11 19:16:01 +0300

    lab01_z24

---
+++ lab01_z08.sh/lab01_z24.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -n $1 ] && [ "$1"  == "-task" ];then
+  echo "Задание"
+  echo "24. Вывести только четные параметры, переданные в скрипт, цикл for записать в одной строке"
+  echo
+  echo
+  fi
+  args=("$@")
+  last =$(($# - 1))
+  for i in `seq 0 $last`; do mod=$(($i%2))  if [ $mod == 1 ]; then  echo ${args[$i]};fi; done 
+
+exit 0
